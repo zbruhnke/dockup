32009c86c0220fa92b3bc1cb5f2240f419a463b3
Emil Soman <emil.soman@gmail.com>
2018-01-16 14:57:11 +0530

    Add support for .dockup.json config file inside projects

--- apps/dockup/lib/dockup/container.ex
+++ apps/dockup/lib/dockup/container.ex
@@ -1,13 +1,40 @@
 defmodule Dockup.Container do
   require Logger
 
+  alias Dockup.{
+    Project,
+    DockerComposeConfig
+  }
+
   def start_containers(project_id, command \\ Dockup.Command) do
     Logger.info "Starting containers of project #{project_id}"
-    command.run("docker-compose", ["-p", "#{project_id}", "up", "-d"], Dockup.Project.project_dir(project_id))
+    command.run(
+      "docker-compose",
+      [
+        "-f", "#{docker_compose_file(project_id)}",
+        "-p", "#{project_id}",
+        "up",
+        "-d"
+      ],
+      Project.project_dir(project_id)
+    )
   end
 
   def stop_containers(project_id, command \\ Dockup.Command) do
     Logger.info "Stopping deployment-#{project_id} containers"
-    command.run("docker-compose", ["-p", "#{project_id}", "down", "-v"], Dockup.Project.project_dir(project_id))
+    command.run(
+      "docker-compose",
+      [
+        "-f", "#{docker_compose_file(project_id)}",
+        "-p", "#{project_id}",
+        "down",
+        "-v"
+      ],
+      Project.project_dir(project_id)
+    )
+  end
+
+  defp docker_compose_file(project_id) do
+    DockerComposeConfig.compose_file_name(project_id)
   end
 end
