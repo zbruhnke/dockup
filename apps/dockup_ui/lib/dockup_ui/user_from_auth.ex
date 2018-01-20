accf58eda835fb0cc1f89bedc3ed35aac5108c5a
Emil Soman <emil.soman@gmail.com>
2018-01-21 02:44:37 +0530

    Add User schema

--- apps/dockup_ui/lib/dockup_ui/user_from_auth.ex
+++ apps/dockup_ui/lib/dockup_ui/user_from_auth.ex
@@ -6,13 +6,30 @@ defmodule DockupUi.UserFromAuth do
   require Poison
 
   alias Ueberauth.Auth
+  alias DockupUi.{
+    User,
+    Repo
+  }
 
-  def find_or_create(%Auth{} = auth) do
-    {:ok, basic_info(auth)}
+  def find_or_create(%Auth{info: %{email: email, name: name}}) do
+    case Repo.get_by(User, email: email) do
+      nil ->
+        create_user(%{email: email, name: name})
+      user ->
+        {:ok, user}
+    end
   end
 
-  defp basic_info(auth) do
-    IO.inspect auth
-    auth
+  defp create_user(params) do
+    changeset = User.changeset(%User{}, params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        {:ok, user}
+      {:error, _} ->
+        error = "User cannot be created with params: #{inspect params}"
+        Logger.error(error)
+        {:error, error}
+    end
   end
 end
