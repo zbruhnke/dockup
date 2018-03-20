0ae2a24f5c13b1dcb214503ae076dd7927c72f8f
Emil Soman <emil.soman@gmail.com>
2018-03-20 17:24:17 +0530

    Show nav link to access dockup logs

--- apps/dockup_ui/web/views/layout_view.ex
+++ apps/dockup_ui/web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule DockupUi.LayoutView do
   use DockupUi.Web, :view
+
+  def log_path do
+    domain = Application.fetch_env!(:dockup, :domain)
+    "//logio.#{domain}/#?projectName=dockup"
+  end
 end
