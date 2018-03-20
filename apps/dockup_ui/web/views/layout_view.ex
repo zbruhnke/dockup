1bed5a2122c0d07ba78f4f3f0ed3b06b2c9b3fa8
Emil Soman <emil.soman@gmail.com>
2018-03-20 17:33:04 +0530

    Use _url since it returns a full url to access dockup logs

--- apps/dockup_ui/web/views/layout_view.ex
+++ apps/dockup_ui/web/views/layout_view.ex
@@ -1,7 +1,7 @@
 defmodule DockupUi.LayoutView do
   use DockupUi.Web, :view
 
-  def log_path do
+  def dockup_log_url do
     domain = Application.fetch_env!(:dockup, :domain)
     "//logio.#{domain}/#?projectName=dockup"
   end
