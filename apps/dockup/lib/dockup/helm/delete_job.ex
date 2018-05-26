a83690a69a58efffc0ecfd464154f3d31488dbcc
Yuva <yuva@codemancers.com>
2018-05-26 16:40:40 +0700

    add helm backend and implement the spec

---
+++ apps/dockup/lib/dockup/helm/delete_job.ex
@@ -0,0 +1,38 @@
+defmodule Dockup.Helm.DeleteJob do
+  require Logger
+
+  alias Dockup.{
+    DefaultCallback,
+    Project,
+    Command
+  }
+
+  def spawn_process(id, callback) do
+    spawn(fn -> perform(id, callback) end)
+  end
+
+  def perform(project_identifier, callback \\ DefaultCallback.lambda, deps \\ []) do
+    IO.inspect project_identifier
+    callback.(:deleting_deployment, nil)
+
+    project = deps[:project] || Project
+    project_id = to_string(project_identifier)
+
+    {_, 0} = Command.run("helm", ["delete", project_id], ".")
+    project.delete_repository(project_id)
+
+    callback.(:deployment_deleted, nil)
+  rescue
+    exception ->
+      stacktrace = System.stacktrace
+      message = Exception.message(exception)
+      handle_error_message(callback, project_identifier, message)
+      reraise(exception, stacktrace)
+  end
+
+  defp handle_error_message(callback, project_identifier, message) do
+    message = "An error occured when deleting deployment #{project_identifier} : #{message}"
+    Logger.error message
+    callback.(:delete_deployment_failed, message)
+  end
+end
---
+++ apps/dockup/lib/dockup/helm/install_job.ex
@@ -0,0 +1,64 @@
+defmodule Dockup.Helm.InstallJob do
+  require Logger
+
+  alias Dockup.{
+    DefaultCallback,
+    Project,
+    Command
+  }
+
+  def spawn_process(%{id: id, git_url: repository, branch: branch}, callback) do
+    spawn(fn -> perform(id, repository, branch, callback) end)
+  end
+
+  def perform(project_identifier, repository, branch,
+              callback \\ DefaultCallback.lambda, deps \\ []) do
+    project    = deps[:project]    || Project
+
+    project_id = to_string(project_identifier)
+
+    callback.(:cloning_repo, nil)
+    project.clone_repository(project_id, repository, branch)
+
+    callback.(:starting, nil)
+    # name = ?a..?z |> Enum.take_random(len) |> to_string
+    name = project_id
+    dir = Project.project_dir(project_id)
+    tag = "tag"
+    {_, 0} = helm_run(dir, ["install",
+                            "--set", "image.tag=#{tag}",
+                            "--name=#{name}",
+                            "."])
+
+    domain = Application.fetch_env!(:dockup, :domain)
+    urls = [name <> "." <> domain]
+    callback.(:checking_urls, log_url(project_id))
+    urls = project.wait_till_up(urls, project_id)
+
+    callback.(:started, urls)
+  rescue
+    exception ->
+      stacktrace = System.stacktrace
+      message = Exception.message(exception)
+      handle_error_message(callback, project_identifier, message)
+      reraise(exception, stacktrace)
+  end
+
+  defp helm_run(dir, command) do
+    Command.run("helm", command, dir)
+  end
+
+  # This should be something else! Maybe logging should be separate framework
+  # We can use ELK and get logging out. K8s has out of box fluentd, not sure
+  # what that is, it can be used for logging
+  defp log_url(project_id) do
+    domain = Application.fetch_env!(:dockup, :domain)
+    "logio.#{domain}/#?projectName=#{project_id}"
+  end
+
+  defp handle_error_message(callback, project_identifier, message) do
+    message = "An error occured when deploying #{project_identifier} : #{message}"
+    Logger.error message
+    callback.(:deployment_failed, message)
+  end
+end
