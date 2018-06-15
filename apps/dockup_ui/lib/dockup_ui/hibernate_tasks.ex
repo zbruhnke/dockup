e741b2f8d8c9533dd0dab1ef11e7ad5790e4f1f6
Yuva <yuva@codemancers.com>
2018-06-15 07:20:32 +0530

    use backend directly instead of the service for hibernating and waking up

--- apps/dockup_ui/lib/dockup_ui/hibernate_tasks.ex
+++ apps/dockup_ui/lib/dockup_ui/hibernate_tasks.ex
@@ -1,23 +1,32 @@
 defmodule DockupUi.HibernateTasks do
   require Ecto.Query
 
-  @start_apps [
-    :crypto,
-    :ssl,
-    :postgrex,
-    :ecto,
-    :dockup_ui
-  ]
-
   def hibernate_all do
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
+    {:ok, _} = Application.ensure_all_started(:dockup_ui)
 
-    deployed_deployments =
-      DockupUi.Deployment
-      |> Ecto.Query.where(status: "started")
-      |> DockupUi.Repo.all
+    IO.inspect Application.started_applications
 
-    ids = Enum.map(deployed_deployments, fn i -> i.id end)
-    DockupUi.HibernateDeploymentService.run_all(ids)
+    statuses = ["started", "hibernating_deployment"]
+    DockupUi.Deployment
+    |> Ecto.Query.where([d], d.status in ^statuses)
+    |> DockupUi.Repo.all
+    |> Enum.map(&hibernate/1)
+
+    :timer.sleep(50000)
+    IO.inspect "stopping everything"
+    :init.stop()
+  end
+
+  defp hibernate(deployment) do
+    backend = Application.fetch_env!(:dockup_ui, :backend_module)
+    backend.hibernate(deployment.id, fn (event, _payload) ->
+      IO.inspect event
+      repo = Application.get_env(:dockup_ui, :ecto_repos, []) |> List.first
+      IO.inspect repo.start_link(pool_size: 1)
+
+      status = Atom.to_string(event)
+      changeset = DockupUi.Deployment.status_changeset(deployment, status)
+      {:ok, _} = DockupUi.Repo.update(changeset)
+    end)
   end
 end
