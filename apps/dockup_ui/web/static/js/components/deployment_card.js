e04225116ba31113d90c32590e8c5453768275a7
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-09-04 22:01:00 +0530

    Disable deploy button and show loading spinner for starting, pending and queued states

--- apps/dockup_ui/web/static/js/components/deployment_card.js
+++ apps/dockup_ui/web/static/js/components/deployment_card.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import TimeAgo from 'react-timeago';
-import DeploymentStatus from './deployment_status';
-import {getStatusColorClass} from '../status_colors';
-import FlashMessage from '../flash_message';
-import ContainersSection from './containers_section';
+import React, {Component} from "react";
+import TimeAgo from "react-timeago";
+import DeploymentStatus from "./deployment_status";
+import {getStatusColorClass} from "../status_colors";
+import FlashMessage from "../flash_message";
+import ContainersSection from "./containers_section";
 
 class DeploymentCard extends Component {
   constructor(props) {
@@ -16,7 +16,7 @@ class DeploymentCard extends Component {
   handleHibernate(e) {
     e.preventDefault();
     let xhr = this.hibernateRequest();
-    xhr.done((response) => {
+    xhr.done(response => {
       this.setState({deployment: Object.assign({}, response.data)});
     });
     xhr.fail(() => {
@@ -27,40 +27,58 @@ class DeploymentCard extends Component {
   hibernateRequest(id) {
     return $.ajax({
       url: `/api/deployments/${this.props.deployment.id}/hibernate`,
-      type: 'PUT',
-      beforeSend: (xhr) => {xhr.setRequestHeader('x-csrf-token', window.csrfToken);},
-      contentType: 'application/json'
+      type: "PUT",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("x-csrf-token", window.csrfToken);
+      },
+      contentType: "application/json"
     });
   }
 
   renderHibernateButton() {
     if (this.props.deployment.status == "started") {
-      return(
-        <button type="button" onClick={this.handleHibernate} className="btn btn-sm btn-outline-primary mr-2">Hibernate</button>
+      return (
+        <button
+          type="button"
+          onClick={this.handleHibernate}
+          className="btn btn-sm btn-outline-primary mr-2"
+        >
+          Hibernate
+        </button>
       );
     }
   }
 
   renderDetailsButton() {
     if (!this.props.showDetails && !(this.props.deployment.status == "deleted")) {
-      return(
-        <a href={`/deployments/${this.props.deployment.id}`} className="btn btn-sm btn-outline-info mr-2">Details</a>
+      return (
+        <a
+          href={`/deployments/${this.props.deployment.id}`}
+          className="btn btn-sm btn-outline-info mr-2"
+        >
+          Details
+        </a>
       );
     }
   }
 
   renderContainersSection() {
-    if(!this.props.showDetails) {
+    if (!this.props.showDetails) {
       return null;
     }
 
-    return(<ContainersSection containers={this.props.deployment.containers} deploymentId={this.props.deployment.id}/>);
+    return (
+      <ContainersSection
+        containers={this.props.deployment.containers}
+        deploymentId={this.props.deployment.id}
+      />
+    );
   }
 
   handleWakeUp(e) {
     e.preventDefault();
     let xhr = this.wakeUpRequest();
-    xhr.done((response) => {
+    xhr.done(response => {
       this.setState({deployment: Object.assign({}, response.data)});
     });
     xhr.fail(() => {
@@ -71,16 +89,24 @@ class DeploymentCard extends Component {
   wakeUpRequest(id) {
     return $.ajax({
       url: `/api/deployments/${this.props.deployment.id}/wake_up`,
-      type: 'PUT',
-      beforeSend: (xhr) => {xhr.setRequestHeader('x-csrf-token', window.csrfToken);},
-      contentType: 'application/json'
+      type: "PUT",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("x-csrf-token", window.csrfToken);
+      },
+      contentType: "application/json"
     });
   }
 
   renderWakeUpButton() {
     if (this.props.deployment.status == "hibernated") {
-      return(
-        <button type="button" onClick={this.handleWakeUp} className="btn btn-sm btn-outline-primary mr-2">Wake Up</button>
+      return (
+        <button
+          type="button"
+          onClick={this.handleWakeUp}
+          className="btn btn-sm btn-outline-primary mr-2"
+        >
+          Wake Up
+        </button>
       );
     }
   }
@@ -88,7 +114,7 @@ class DeploymentCard extends Component {
   handleDelete(e) {
     e.preventDefault();
     let xhr = this.deleteRequest();
-    xhr.done((response) => {
+    xhr.done(response => {
       this.setState({deployment: Object.assign({}, response.data)});
     });
     xhr.fail(() => {
@@ -99,16 +125,24 @@ class DeploymentCard extends Component {
   deleteRequest(id) {
     return $.ajax({
       url: `/api/deployments/${this.props.deployment.id}`,
-      type: 'DELETE',
-      beforeSend: (xhr) => {xhr.setRequestHeader('x-csrf-token', window.csrfToken);},
-      contentType: 'application/json'
+      type: "DELETE",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("x-csrf-token", window.csrfToken);
+      },
+      contentType: "application/json"
     });
   }
 
   renderDeleteButton() {
-    if(this.props.deployment.status != "deleted" && this.props.deployment.status != "deleting") {
-      return(
-        <button type="button" onClick={this.handleDelete} className="btn btn-sm  btn-outline-danger">Delete</button>
+    if (this.props.deployment.status != "deleted" && this.props.deployment.status != "deleting") {
+      return (
+        <button
+          type="button"
+          onClick={this.handleDelete}
+          className="btn btn-sm  btn-outline-danger"
+        >
+          Delete
+        </button>
       );
     }
   }
@@ -117,7 +151,7 @@ class DeploymentCard extends Component {
     let statusClass = getStatusColorClass(this.props.deployment.status);
     let borderClass = `border-${statusClass}`;
     let textClass = `text-${statusClass}`;
-    return(
+    return (
       <div className="mb-5">
         <div className={`card ${borderClass} ${textClass} dockup-card`}>
           <div className="card-body">
@@ -125,19 +159,20 @@ class DeploymentCard extends Component {
               <div className="col-8">
                 <h4 className="card-title display-4 dockup-card-branch">
                   {this.props.deployment.name}
-                  <i className="fa fa-code-branch"/>
+                  <i className="fa fa-code-branch" />
                 </h4>
               </div>
               <div className="col text-right dockup-card-timeago">
-                <TimeAgo date={this.props.deployment.inserted_at} title={new Date(this.props.deployment.inserted_at)}/>
+                <TimeAgo
+                  date={this.props.deployment.inserted_at}
+                  title={new Date(this.props.deployment.inserted_at)}
+                />
               </div>
-
             </div>
 
             <h6 className="card-subtitle mb-2">{this.props.deployment.blueprint_name}</h6>
 
             {this.renderContainersSection()}
-
           </div>
           <div className="card-footer dockup-card-footer">
             <div className="row">
@@ -148,7 +183,7 @@ class DeploymentCard extends Component {
                 {this.renderDeleteButton()}
               </div>
 
-              <DeploymentStatus status={this.props.deployment.status}/>
+              <DeploymentStatus status={this.props.deployment.status} />
             </div>
           </div>
         </div>
