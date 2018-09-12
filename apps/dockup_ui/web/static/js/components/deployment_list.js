f3e9afdf343cebeb3a3350943ab12ffade0ec444
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-09-12 15:57:20 +0530

    Preserve Page no, filter and items per page on page refresh

--- apps/dockup_ui/web/static/js/components/deployment_list.js
+++ apps/dockup_ui/web/static/js/components/deployment_list.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
+import createHistory from "history/createBrowserHistory"
 import DeploymentCard from './deployment_card';
 
 class DeploymentList extends Component {
   constructor(props) {
     super(props);
+    this.history = createHistory()
     this.state = {
       deployments: [],
       filteredDeployments : [],
       currentPageDeployments:[],
       filterBy: "all",
       currentPage: 1,
-      itemsPerPage: 10
+      itemsPerPage: 10,
     }
     this.getDeployments();
     this.connectToDeploymentsChannel();
     this.handleFilterChange = this.handleFilterChange.bind(this)
     this.changeitemsPerPage = this.changeitemsPerPage.bind(this)
+  }
+
+  componentDidMount(){
+      if(this.history.location.state){
+        const {currentPage, filterBy, itemsPerPage} = this.history.location.state
+        this.setState({currentPage, filterBy, itemsPerPage})
+        this.filterDeployments();
+      }
   }
 
   getDeployments() {
@@ -24,7 +34,7 @@ class DeploymentList extends Component {
     xhr.onload = () => {
       if (xhr.status === 200) {
         let deployments = JSON.parse(xhr.responseText).data;
-        this.setState({deployments}, () => { this.filterDeployments() });
+        this.setState({deployments}, () => { this.filterDeployments()});
       }
     };
     xhr.send();
@@ -32,7 +42,10 @@ class DeploymentList extends Component {
 
   addDeployment(deployment) {
     this.setState({deployments: [deployment, ...this.state.deployments]},
-      () => {this.filterDeployments() })
+      () => {
+        this.filterDeployments()
+        this.pushHistory()
+      })
   }
 
   connectToDeploymentsChannel() {
@@ -63,7 +76,16 @@ class DeploymentList extends Component {
   }
 
   handleFilterChange(event){
-    this.setState({filterBy: event.target.value, currentPage: 1}, () => {this.filterDeployments()})
+    this.setState({filterBy: event.target.value, currentPage: 1},
+      () => {
+        this.filterDeployments()
+        this.pushHistory()
+      })
+  }
+
+  pushHistory(){
+    const {filterBy,currentPage, itemsPerPage} = this.state
+    this.history.push(`/deployments?filter=${filterBy}?pageNo=${currentPage}?items=${itemsPerPage}`, {filterBy, currentPage, itemsPerPage})
   }
 
   chunkDeployments(array, chunk_size){
@@ -90,7 +112,7 @@ class DeploymentList extends Component {
   renderFilter(){
     return (<div className="form-group col">
     <label className="my-1 mr-2" htmlFor="filter-deployments">Filter deployments</label>
-    <select className="custom-select-sm my-3 mr-sm-2 " value={this.state.value} onChange={this.handleFilterChange}>
+    <select className="custom-select-sm my-3 mr-sm-2 " value={this.state.filterBy} onChange={this.handleFilterChange}>
       <option value="all">all</option>
       <option value="queued">queued</option>
       <option value="started">started</option>
@@ -110,14 +132,17 @@ class DeploymentList extends Component {
 
   changeitemsPerPage(e){
     const itemsPerPage = parseInt(e.target.value)
-    this.setState({itemsPerPage}, () => {this.filterDeployments()})
+    this.setState({itemsPerPage}, () => {
+      this.filterDeployments()
+      this.pushHistory()
+    })
   }
 
   renderPaginationOptions(){
     return (
     <div className="form-group col">
     <label className="my-1 mr-2" htmlFor="pagination-options">Items per page</label>
-    <select className="custom-select-sm my-3 mr-sm-2 " value={this.state.itemsOnPage} onChange={this.changeitemsPerPage}>
+    <select className="custom-select-sm my-3 mr-sm-2 " value={this.state.itemsPerPage} onChange={this.changeitemsPerPage}>
       <option value="10">10</option>
       <option value="20">20</option>
       <option value="30">30</option>
