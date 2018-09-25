d86739f9c303f1b34be9afa976372b0a7c44098c
Atul Bhosale <atul1bhosale@gmail.com>
2018-09-25 13:47:59 +0530

    Use name variable for filename parameter to MiniCssExtractPlugin instance

--- apps/dockup_ui/assets/webpack.config.js
+++ apps/dockup_ui/assets/webpack.config.js
@@ -43,7 +43,7 @@ module.exports = (env, options) => ({
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "[app].css" }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
